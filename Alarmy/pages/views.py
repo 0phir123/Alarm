b3e5ae83b77200afb7368279257f6b10797fdb11
0phir123 <ophirackerman@gmail.com>
2020-12-03 23:58:00 +0200

    change the views.py

--- Alarmy/pages/views.py
+++ Alarmy/pages/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
+    if request == 'POST':
+        print (request.POST[''])
+
     return render(request, 'pages/index.html')
 def about(request):
     return render(request, 'pages/about.html')
