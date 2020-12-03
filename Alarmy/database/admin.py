70aa5510344d34e99a19afb78cb8cc773ab8c030
0phir123 <ophirackerman@gmail.com>
2020-12-03 23:14:03 +0200

    first commit

---
+++ Alarmy/database/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Database
+
+
+class DatabaseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'trigger', 'time')
+    list_display_links = ('id','time')
+    search_fields = ('time',)
+    list_per_page = 25
+
+admin.site.register(Database, DatabaseAdmin)
---
+++ Alarmy/database/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+class Database(models.Model):
+   
+    trigger = models.BooleanField(default=False)
+    time = models.DateTimeField(default=datetime.now)
+    
+    def __str__(self):
+        return str(self.time)
